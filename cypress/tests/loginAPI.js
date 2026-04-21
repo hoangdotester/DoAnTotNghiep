863654948f12722d1f5afb9ed3950b8682099e1f
HoangDo <hoangdonguyen81100@gmail.com>
2026-04-21 13:44:19 +0700

    add verify reset-password script

--- cypress/tests/loginAPI.js
+++ cypress/tests/loginAPI.js
@@ -1,6 +1,23 @@
 /// <reference types='cypress' />
 
 describe('Testing auth-controller ', () => {
+    it('Verify that forgot-password API working with valid Email ', () => {
+        cy.fixture("users").then((user) => {
+            const userData = Object.keys(user.USER);
+            const randomUser = user.USER[userData[Math.floor(Math.random() * userData.length)]];
+            cy.request({
+                method: 'post',
+                url: '/api/auth/forgot-password',
+                qs: {
+                    email: randomUser.email,
+                },
+            }).then((res) => {
+                expect(res.status).to.eq(200);
+                cy.log(randomUser.email)
+            })
+        })
+    })
+
     it('Login susseccfully, response 200 and role ADMIN', () => {
         cy.fixture("users").then((user) => {
             const userData = Object.keys(user.ADMIN);
@@ -116,29 +133,47 @@ describe('Testing auth-controller ', () => {
         })
     })
 
-    it('Verify that forgot-password API working with valid Email ', () => {
+    it('Verify that OTP send from API Forgot-password is working ', () => {
         cy.fixture("users").then((user) => {
             const userData = Object.keys(user.USER);
             const randomUser = user.USER[userData[Math.floor(Math.random() * userData.length)]];
-            cy.request({
-                method: 'post',
-                url: '/api/auth/forgot-password',
-                qs: {
-                    email: randomUser.email,
-                },
-            }).then((res) => {
-                expect(res.status).to.eq(200);
-                cy.log(randomUser.email)
+            cy.wait(5000)
+            cy.sendOTP(randomUser.email);
+            cy.wait(2000);
+            cy.getOTPFromMailtrap(randomUser.email).then((otp) => {
+                cy.log('OTP: ' + otp);
+                expect(otp).to.have.length(6);
             })
         })
     })
 
-    it.only('Verify that OTP send from API Forgot-password is working ', () => {
-        cy.sendOTP('user01@gmail.com');
-        cy.wait(2000);
-        cy.getOTPFromMailtrap('user01@gmail.com').then((otp) => {
-            cy.log('OTP: ' + otp);
-            expect(otp).to.have.length(6);
+    it.only('Verify that the reset password API is working well', () => {
+        cy.fixture('users').then((user) => {
+            const data = Object.keys(user.ADMIN)[0];
+            cy.sendOTP(user.ADMIN[data].email);
+            cy.getOTPFromMailtrap(user.ADMIN[data].email).then((otp) => {
+                cy.request({
+                    method: 'post',
+                    url: '/api/auth/reset-password',
+                    body: {
+                        email: user.ADMIN[data].email,
+                        otp: otp,
+                        newPassword: 'Admin@123'
+                    }
+                }).then((res) => {
+                    expect(res.status).to.eq(200);
+                    cy.request({
+                        method: 'post',
+                        url: '/api/auth/login',
+                        body: {
+                            username: 'admin',
+                            password: 'Admin@123'
+                        }
+                    }).then((res) => {
+                        expect(res.status).to.eq(200);
+                    })
+                })
+            })
         })
     })
 })
